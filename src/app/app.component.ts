90a9017fb1ed419dede90d66f2db359a7d91a187
David Hawkins <suppliers@paradoxit.co.uk>
2017-07-21 10:03:05 +0100

    added sample async data loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,51 +3,92 @@ import { Component } from '@angular/core';
 declare var Stimulsoft: any;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+	title = 'app';
 
-  viewer: any = new Stimulsoft.Viewer.StiViewer(null, 'StiViewer', false);
-  report: any = new Stimulsoft.Report.StiReport();
-  dataSet: any = new Stimulsoft.System.Data.DataSet("Dataset1");
+	viewer: any = new Stimulsoft.Viewer.StiViewer(null, 'StiViewer', false);
+	report: any = new Stimulsoft.Report.StiReport();
+	dataSet: any = new Stimulsoft.System.Data.DataSet("Dataset1");
 
-  ngAfterViewInit() {
+	ngAfterViewInit() {
 
-    this.loadReport();
-    this.viewer.renderHtml('viewer');
-  }
+		this.loadReport();
+		this.viewer.renderHtml('viewer');
+	}
 
-  ngOnInit() {
+	ngOnInit() {
 
-  }
+	}
 
-  loadReport(): void {
+	loadReport(): void {
 
+		let self = this;
+		this.report.loadFile('/assets/reports/testreport.mrt');
+		//self.report.dictionary.variables.getByName("ReportTitleLabel").valueObject = this.l("VisitExceptionsReport");
 
-    let self = this;
-    this.report.loadFile('/assets/reports/testreport.mrt');
-    //self.report.dictionary.variables.getByName("ReportTitleLabel").valueObject = this.l("VisitExceptionsReport");
+		this.viewer.onInteraction = function(e)
+        {
+           self.loadReportData(e.variables.IntegerInput);
 
+        };
 
-    // this.loadReportData(moment().subtract(6, 'days').startOf('day'),
-    //   moment().endOf('day'),
-    //   10);
-
-    this.viewer.report = this.report;
-
-    // this.viewer.onInteraction = function (e) {
-    //   self.loadReportData(moment(e.variables.StartDateTime),
-    //     moment(e.variables.EndDateTime),
-    //     e.variables.MaxVisits);
-
-    // };
+		this.viewer.report = this.report;
 
 
+		this.loadReportData(10); // default load
+		// this.viewer.onInteraction = function (e) {
+		//   self.loadReportData(moment(e.variables.StartDateTime),
+		//     moment(e.variables.EndDateTime),
+		//     e.variables.MaxVisits);
+
+		// };
 
 
-  }
+
+
+	}
+
+	loadReportData(integerInput) {
+		console.log('loading report (integerInput=' + integerInput + ')')
+		let self = this;
+		self.report.dictionary.databases.clear();
+
+		self.report.dictionary.variables.getByName("IntegerInput").valueObject = integerInput;
+
+		// get data (simulate taking seconds)
+
+		console.log('start loading data');
+		setTimeout(function () {
+			console.log('end loading data');
+			var jsonArray = [];
+
+			for (var i = 0; i < integerInput; i++) {
+				jsonArray.push(
+					{
+						userId: i+1,
+						title: "test user " + i+1
+					}
+				);
+			}
+
+			self.bindData(jsonArray);
+
+		}, 5000);
+
+	}
+
+	bindData(jsonArray) {
+		console.log('binding data');
+		this.dataSet = new Stimulsoft.System.Data.DataSet("test123");
+		this.dataSet.readJson(JSON.stringify(jsonArray));
+		this.report.regData("test123", "test123", this.dataSet);
+		this.viewer.renderHtml('viewer');
+		this.report.render(); //whole report refreshes here instead of just showing data
+	}
+
 
 }
